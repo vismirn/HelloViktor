498d3bb2a648c36a722b6908c8a463cf0dc05607
Viktor Smirnov <smirn04@yandex.ru>
2023-09-20 20:30:28 +0300

    Initial Commit,

---
+++ HelloViktor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloViktor
+//
+//  Created by Viktor Smirnov on 20.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
